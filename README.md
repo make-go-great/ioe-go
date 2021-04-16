a341e7009e7ef7a13ccaa60a0fe68c4c2962ccd7
Tran Hau <ngtranhau@gmail.com>
2021-04-17 00:12:41 +0700

    feat: add ReadInput

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# io-go
+# ioe-go
+
+Input output extend.
---
+++ ioe.go
@@ -0,0 +1,24 @@
+package ioe
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+// Read input until newline,
+// ignore empty line
+func ReadInput() string {
+	bs := bufio.NewScanner(os.Stdin)
+	for bs.Scan() {
+		line := bs.Text()
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		return line
+	}
+
+	return ""
+}
